84c7c2dda8cca7ad0dc488d43ef9e48fd9154225
ermakov.i <ilyaermakov710@ya.ru>
2024-11-10 18:17:40 +0300

    feat(factory): add factory processing in multiple threads

--- commands/EventController.php
+++ commands/EventController.php
@@ -4,44 +4,55 @@ namespace app\commands;
 
 use app\components\factories\UserEventFactory;
 use app\components\UserEventPublisher;
+use Redis;
 use yii\console\Controller;
 
 class EventController extends Controller
 {
     private UserEventFactory $userEventFactory;
-    private UserEventPublisher $userEventPublisher;
 
     public function __construct(
         $id,
         $module,
         UserEventFactory $userEventFactory,
-        UserEventPublisher $userEventPublisher,
         $config = []
     )
     {
         $this->userEventFactory = $userEventFactory;
-        $this->userEventPublisher = $userEventPublisher;
         parent::__construct($id, $module, $config);
     }
 
     public function actionIndex(): void
     {
         $userCount = 1000;
-        $eventCount = 100;
+        $eventCount = 1000;
 
         $totalEvents = $userCount * $eventCount;
 
         $this->stdout("Start processing...\n");
 
         for ($userId = 1; $userId <= $userCount; $userId++) {
-            $events = $this->userEventFactory->create($userId, $eventCount);
+            $pid = pcntl_fork();
 
-            foreach ($events as $index => $event) {
-                $this->userEventPublisher->publish($userId, $event);
+            if ($pid == -1) {
+                $this->stderr("Unable to fork process for user $userId.\n");
+                continue;
+            } elseif ($pid == 0) {
+                $events = $this->userEventFactory->create($userId, $eventCount);
+                $client = new Redis();
+                $client->connect('redis', 6379);
+                $userEventPublisher = new UserEventPublisher($client);
+                foreach ($events as $index => $event) {
+                    $userEventPublisher->publish($userId, $event);
 
-                $currentProgress = (($userId - 1) * $eventCount + ($index + 1)) / $totalEvents * 100;
-                $this->stdout(sprintf("\rProgress: %.2f%%", $currentProgress));
+                    $currentProgress = (($userId - 1) * $eventCount + ($index + 1)) / $totalEvents * 100;
+                    $this->stdout(sprintf("\rProgress: %.2f%%", $currentProgress));
+                }
+                exit;
             }
+        }
+
+        while (pcntl_waitpid(0, $status) != -1) {
         }
 
         $this->stdout("\nProcessing completed.\n");
