695ae03e14e2b4c3a01b9ce00383aec80aecf09a
ermakov.i <ilyaermakov710@ya.ru>
2024-11-10 17:00:35 +0300

    init

---
+++ config/console.php
@@ -0,0 +1,31 @@
+<?php
+
+$params = require __DIR__ . '/params.php';
+
+$config = [
+    'id' => 'event-console',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'app\commands',
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+        '@tests' => '@app/tests',
+    ],
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'log' => [
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+    ],
+    'params' => $params,
+];
+
+return $config;
