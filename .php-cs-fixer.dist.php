7719fe1e36d7a6bf6878fe7bc037843a192f34a3
ermakov.i <ilyaermakov710@ya.ru>
2024-11-10 22:59:34 +0300

    feat: add linter php -cs fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+
+return (new Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PHP74Migration' => true,
+        '@PHP74Migration:risky' => true,
+        '@PHPUnit100Migration:risky' => true,
+        '@PhpCsFixer' => true,
+        '@PhpCsFixer:risky' => true,
+        'PhpCsFixerInternal/configurable_fixer_template' => true, // internal rules, shall not be used outside of main repo
+        'general_phpdoc_annotation_remove' => ['annotations' => ['expectedDeprecation']], // one should use PHPUnit built-in method instead
+        'modernize_strpos' => true, // needs PHP 8+ or polyfill
+        'no_useless_concat_operator' => false, // TODO switch back on when the `src/Console/Application.php` no longer needs the concat
+        'numeric_literal_separator' => true,
+    ])
+    ->setFinder(
+        (new Finder())
+            ->ignoreDotFiles(false)
+            ->ignoreVCSIgnored(true)
+            ->exclude(['vendor', 'runtime'])
+            ->in(__DIR__)
+            ->append([__DIR__.'/commands'])
+            ->append([__DIR__.'/components'])
+    )
+    ;
